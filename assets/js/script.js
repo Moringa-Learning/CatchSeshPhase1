bcb2204377f58f147ba9a7928028acebb268ee80
layersony <layersony@gmail.com>
2022-06-16 13:26:41 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const projectUrl = "https://phaseonecatchapi.herokuapp.com/api/v1/projects/"
+  const skillUrl = "https://phaseonecatchapi.herokuapp.com/api/v1/skills/"
+
+
+})
